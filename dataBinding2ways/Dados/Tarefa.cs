f0046dd11e5ab40372a8a3f4fa0add7e93d14079
Silvaj <juan_nascimento@alunos.unicesumar.edu.br>
2022-06-30 16:10:44 -0300

    Adicionar arquivos de projeto.

---
+++ dataBinding2ways/Dados/Tarefa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataBinding2ways.Dados
+{
+    class Tarefa
+    {
+        public string Nome{ get; set; }
+
+        public bool Feito { get; set; }
+
+        public bool Fazendo { get; set; }
+
+        public bool Pendente { get; set; }
+
+        public int GrupoID { get; set;}
+        
+        public Tarefa() { 
+        }
+
+        public Tarefa(string name)
+        {
+            Nome = name;
+        }
+
+        public Tarefa(string name,bool feito, bool fazendo, bool pendente) {
+            Nome = name;
+            Feito = feito;
+            Fazendo = fazendo;
+            Pendente = pendente;
+        }
+    }
+}
---
+++ dataBinding2ways/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using dataBinding2ways.Dados;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection.Emit;
+
+namespace dataBinding2ways
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Tarefa> tarefas = new List<Tarefa>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Insercao_Click(object sender, RoutedEventArgs e)//OK
+        {
+            int quantidadeTarefas = dgTarefas.Items.Count;
+
+            if (quantidadeTarefas < 7)
+            {
+                Tarefa nova = new Tarefa(entrada.Text.ToString(),false,false,false);
+                dgTarefas.Items.Add(nova);
+                tarefas.Add(nova);
+                entrada.Text = string.Empty;
+            }
+            else
+            {
+                entrada.Text = string.Empty;
+                System.Windows.MessageBox.Show("Número máximo de tarefas a cadastrar alcançado!");
+            }
+        }
+
+        private void exclui_Click(object sender, RoutedEventArgs e)
+        {
+            dgTarefas.Items.Remove(dgTarefas.SelectedItem);
+        }
+
+        private void checkFeito(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void checkFazendo(object sender, RoutedEventArgs e) {
+            System.Windows.MessageBox.Show("Clicou em FAZENDO");
+        }
+        private void checkPendente(object sender, RoutedEventArgs e) {
+            System.Windows.MessageBox.Show("Clicou em PENDENTE");
+            
+        }
+    }
+}
